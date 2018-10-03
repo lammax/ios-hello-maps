fd8271d0006af0f7a05033461bb66de97d488a4b
Lammax <lammax@mail.ru>
2018-10-03 12:30:28 +0300

    Show polyline direction on the map

--- hello-maps/ViewController.swift
+++ hello-maps/ViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 import MapKit
 import CoreLocation
+import UserNotifications
 
-
-
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak var mapTypeControl: UISegmentedControl!
     @IBOutlet weak var mapView: MKMapView!
@@ -24,6 +23,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         super.viewDidLoad()
         //TODO - make automatical compas positioning 2D/ I'd like to rotate map according to compas.
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in }
         
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
@@ -51,64 +52,6 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
         
     }
-    
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        if overlay is MKCircle {
-            let circleRenderer = MKCircleRenderer(circle: overlay as! MKCircle)
-            circleRenderer.lineWidth = 1.0
-            circleRenderer.strokeColor = UIColor.purple
-            circleRenderer.fillColor = UIColor.white
-            circleRenderer.alpha = 0.5
-            return circleRenderer
-        }
-        
-        return MKOverlayRenderer()
-    }
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        if annotation is MKUserLocation {
-            return nil
-        }
-        
-        var customAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "CustomAnnotationView")
-        
-        if customAnnotationView == nil {
-            customAnnotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "CustomAnnotationView")
-            customAnnotationView?.canShowCallout = false
-        } else {
-            customAnnotationView?.annotation = annotation
-        }
-        
-        if let customAnnotation = annotation as? CustomAnnotation {
-            customAnnotationView?.image = UIImage(named: customAnnotation.imageURL)
-        }
-        
-        return customAnnotationView
-        
-    }
-    
-    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
-        view.subviews.forEach { (subview) in
-            subview.removeFromSuperview()
-        }
-    }
-    
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        
-        guard let annotation = view.annotation as? CustomAnnotation else {
-            return
-        }
-        
-        //create custom callout
-        CustomCalloutView(annotation: annotation).add(to: view)
-
-    }
-    
-    /*func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-        mapView.setRegion(region, animated: true)
-    }*/
 
     @IBAction func addAnnotationClick(_ sender: UIButton) {
         //CLLocationCoordinate2D(latitude: 55.7525497882909, longitude: 37.6231188699603)
@@ -154,6 +97,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                         } else {
                             self.directionSteps = route.steps
                         }
+                        
+                        self.mapView.addOverlay(route.polyline, level: .aboveRoads)
                         
                     })
                     
@@ -227,7 +172,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         //start to monitor region
         //add region for monitoring
-        let region = CLCircularRegion(center: annotation.coordinate, radius: 500.0, identifier: "Region1")
+        let region = CLCircularRegion(center: annotation.coordinate, radius: 1000.0, identifier: "Region1")
         region.notifyOnEntry = true
         region.notifyOnExit = true
         
@@ -238,26 +183,30 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
     
     private func addFancyRegion(annotation: CustomAnnotation) {
-        self.mapView.addOverlay(MKCircle(center: annotation.coordinate, radius: 500)) //1000 meters
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        popOKAlert(title: "Enter region \(region.identifier)")
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        popOKAlert(title: "Exit region \(region.identifier)")
+        self.mapView.addOverlay(MKCircle(center: annotation.coordinate, radius: 1000)) //1000 meters
     }
     
     private func popOKAlert(title: String?) {
         let alertVC = UIAlertController(title: title, message: nil, preferredStyle: .alert)
         
         let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            print("OK")
         }
         
         alertVC.addAction(okAction)
         
         self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    private func showNotification(title: String, message: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        content.badge = 1
+        content.sound = .default
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
     }
     
 }
@@ -266,4 +215,93 @@ extension ViewController: DirectionsTableDelegate {
     func getDirectionSteps() -> [MKRoute.Step] {
         return self.directionSteps
     }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+        mapView.setRegion(region, animated: true)
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        showNotification(title: "Enter", message: "Enter region \(region.identifier)")
+        popOKAlert(title: "Pop Enter region \(region.identifier)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        showNotification(title: "Exit", message: "Exit region \(region.identifier)")
+        popOKAlert(title: "Pop Exit region \(region.identifier)")
+    }
+
+}
+
+extension ViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        //print(overlay.title)
+        
+        if overlay is MKCircle {
+            let circleRenderer = MKCircleRenderer(circle: overlay as! MKCircle)
+            circleRenderer.lineWidth = 1.0
+            circleRenderer.strokeColor = UIColor.purple
+            circleRenderer.fillColor = UIColor.white
+            circleRenderer.alpha = 0.5
+            return circleRenderer
+        }
+        
+        if overlay is MKPolyline {
+            let polylineRenderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+            polylineRenderer.lineWidth = 3.0
+            polylineRenderer.strokeColor = UIColor.purple
+            polylineRenderer.fillColor = UIColor.white
+            polylineRenderer.alpha = 0.5
+            return polylineRenderer
+        }
+        
+        return MKOverlayRenderer()
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        var customAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "CustomAnnotationView")
+        
+        if customAnnotationView == nil {
+            customAnnotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "CustomAnnotationView")
+            customAnnotationView?.canShowCallout = false
+        } else {
+            customAnnotationView?.annotation = annotation
+        }
+        
+        if let customAnnotation = annotation as? CustomAnnotation {
+            customAnnotationView?.image = UIImage(named: customAnnotation.imageURL)
+        }
+        
+        return customAnnotationView
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        view.subviews.forEach { (subview) in
+            subview.removeFromSuperview()
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        guard let annotation = view.annotation as? CustomAnnotation else {
+            return
+        }
+        
+        //create custom callout
+        CustomCalloutView(annotation: annotation).add(to: view)
+        
+    }
+    
 }
